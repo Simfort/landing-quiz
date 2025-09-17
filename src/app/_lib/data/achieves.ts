f558c7fd5fb0274036d703953431cbdcec1fd40e
Simfort <lemondzavadavid265@gmail.com>
2025-09-17 17:36:39 +0300

    changed

--- src/app/_lib/data/achieves.ts
+++ src/app/_lib/data/achieves.ts
@@ -2,7 +2,7 @@ const ACHIEVES = [
   "Learn how to locate and train your pelvic floor muscles the right way.",
   "Get access to Kegel routines designed to support intimate health.",
 
-  "Explore in-app articles with clear answers to common male health questions.",
+  "Explore in-app tips with clear answers to common male health questions.",
 ];
 
 export default ACHIEVES;
